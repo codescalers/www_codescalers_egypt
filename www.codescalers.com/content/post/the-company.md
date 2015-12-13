2d24d67be676c53c567b238ffadc903a9f6518f6
magedf <magedf@codescalers.com>
2015-12-13 18:15:57 +0200

    Prepare the theme for hugo

---
+++ www.codescalers.com/content/post/the-company.md
@@ -0,0 +1,7 @@
++++
+#Don't remove title!
+title = "The Company"
+hello = "HESBES"
++++
+
+
---
+++ www.codescalers.com/themes/landing/static/js/general.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+
+$('.panel-collapse').on('show.bs.collapse',function(){
+    $(this).prev('.panel-heading').addClass("active");
+});
+
+$('.panel-collapse').on('hide.bs.collapse',function(){
+    $(this).prev('.panel-heading').removeClass("active");
+});
+
+
+
+
+  var str=location.href.toLowerCase();
+    $('li.active').removeClass("active");
+  $('.nav li a').filter(function() {return this.href.toLowerCase() == str; }).parents('li').addClass('active');
+     
+
+
+
+
+
+
+function reduceMenu() {
+$('#mainMenu').addClass('scroll');
+}
+
+function expandMenu() {
+$('#mainMenu').removeClass('scroll');
+}
+
+$(window).on('scroll', function(){
+if ($(window).scrollTop() > 81) {
+  reduceMenu();
+} else {
+  expandMenu();
+}
+});
+
+      
+    
+
+    
+
+    
+});
